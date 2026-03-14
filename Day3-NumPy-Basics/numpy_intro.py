40cf555d189b6195d3dba66e31b1185ad9da709d
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-14 20:15:46 +0530

    Update numpy_intro.py

--- Day3-NumPy-Basics/numpy_intro.py
+++ Day3-NumPy-Basics/numpy_intro.py
@@ -1,4 +1,20 @@
+#prgrm10: Basic Numpy Operation
 import numpy as np
 arr = np.array([1,2,3,4,5])
 print(arr)
-print(arr + 5)   #vectorized operation
+print(f"Vectorized operation: {arr + 5}")   
+import numpy as np
+data = np.array([10,20,30,40,50])
+print(f"Mean of data: {np.mean(data)}")
+print("Max of data:", np.max(data))
+print("Min of data:", np.min(data))
+import numpy as np
+matrix = np.array([
+  [1,2,3]
+  [4,5,6]
+])
+print(f"Matrix: {matrix}")
+print("Matrix shape:", matrix.shape)
+ipmort numpy as np
+random_numbers = np.random.rand(5)
+print(f"5 Random numbers: {random_numbers}")
