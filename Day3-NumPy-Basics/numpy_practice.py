f76342991214ed62520856066d35ac59b44f0454
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-14 20:40:00 +0530

    Create numpy_practice.py

---
+++ Day3-NumPy-Basics/numpy_practice.py
@@ -0,0 +1,7 @@
+#prgrm11: Practice Program Using NumPy Array
+import numpy as np
+p_arr = np.array([5,10,15,20,25])
+print(f"Practice Array: {p_arr}")
+print(f"Mean: {np.mean(p_arr)}")
+print(f"Max: {np.max(p_arr)}")
+print(f"Array after 2*: {p_arr*2}")
