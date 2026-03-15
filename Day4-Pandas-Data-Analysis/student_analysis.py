4c075ddba8291b95d4b6670cbc45a294d7d74b1e
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-15 22:03:34 +0530

    Update student_analysis.py

--- Day4-Pandas-Data-Analysis/student_analysis.py
+++ Day4-Pandas-Data-Analysis/student_analysis.py
@@ -1,8 +1,14 @@
+#prgrm13: Practice using Pandas and MatplotLib
 import pandas as pd
 data = pd.read_csv(r"C:\Users\KIIT0001\Desktop\AI-Internship -Journey-2026\Day4-Pandas-Data-Analysis\students.csv")
 print(f"Student data: {data}")
-print(f"Data head: {(data.head())}")
-print(f"Data info: {(data.info())}")
-print(f"Data describe: {(data.describe())}")
-print("Math columns:", (data["math"]))
-                    
+print(f"Data head:\n {(data.head())}")
+print(f"Data info:\n {(data.info())}")
+print(f"Data describe:\n {(data.describe())}")
+print("Math columns:\n", (data["math"]))
+print("Sort by math score:\n", data.sort_values(by="math", ascending= False)) 
+print("Average science score:\n", (data["science"].mean))
+import matplotlib.pyplot as plt
+data.plot(x="name", y=["math","science","english"], kind="bar")
+plt.title("Student scores")
+plt.show()
