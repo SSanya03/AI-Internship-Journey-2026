baf501f79968e62b9931e59a1c2628fe4e28d323
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-15 21:36:52 +0530

    Update student_analysis.py

--- Day4-Pandas-Data-Analysis/student_analysis.py
+++ Day4-Pandas-Data-Analysis/student_analysis.py
@@ -4,5 +4,5 @@ print(f"Student data: {data}")
 print(f"Data head: {(data.head())}")
 print(f"Data info: {(data.info())}")
 print(f"Data describe: {(data.describe())}")
-print("Math columns:", (data[math]))
+print("Math columns:", (data["math"]))
                     
