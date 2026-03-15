16558e955ddf830e34cc953b8d0a24abf977f5b6
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-15 21:47:51 +0530

    Update student_analysis.py

--- Day4-Pandas-Data-Analysis/student_analysis.py
+++ Day4-Pandas-Data-Analysis/student_analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data = pd.read_csv("students.csv")
+data = pd.read_csv(r"C:\Users\KIIT0001\Desktop\AI-Internship -Journey-2026\Day4-Pandas-Data-Analysis\students.csv")
 print(f"Student data: {data}")
 print(f"Data head: {(data.head())}")
 print(f"Data info: {(data.info())}")
