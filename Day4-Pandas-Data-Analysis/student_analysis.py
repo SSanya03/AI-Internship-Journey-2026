19809f4638c372c676f021336a643be288d16057
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-15 21:34:30 +0530

    Create student_analysis.py

---
+++ Day4-Pandas-Data-Analysis/student_analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = pd.read_csv("students.csv")
+print(f"Student data: {data}")
+print(f"Data head: {(data.head())}")
+print(f"Data info: {(data.info())}")
+print(f"Data describe: {(data.describe())}")
+print("Math columns:", (data[math]))
+                    
