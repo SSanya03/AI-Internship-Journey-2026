aaa9de1e2b51f1acd5afb7158c8d530336145465
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-16 21:38:17 +0530

    Create visualization_practice.py

---
+++ Day5-Data-Visualization/visualization_practice.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv(r"C:\Users\KIIT0001\Desktop\AI-Internship -Journey-2026\Day4-Pandas-Data-Analysis\students.csv")
+print("Student Marks:\n", data)
+data["math"].plot()
+plt.show()
