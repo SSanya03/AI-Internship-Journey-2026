0cf526f52771039f5e9b3750cf3395e342b2dfab
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-17 18:41:54 +0530

    Update ml_model.py

--- Day6-Machine-Learning/ml_model.py
+++ Day6-Machine-Learning/ml_model.py
@@ -1,8 +1,10 @@
+#prgrm15: First ML model
 import pandas as pd
 data = pd.read_csv(r"C:\Users\KIIT0001\Desktop\AI-Internship -Journey-2026\Day6-Machine-Learning\students.csv")
 print("Students data:\n", data)
+data["total"] = data["math"] + data["sciene"] + data["english"]
 x = data[["math", "science", "english"]]
-y = data["math"]
+y = data["total"]
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(x,y)
