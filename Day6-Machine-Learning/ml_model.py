77b751a02d484fe6c1ae13b352b93fc518e5cb88
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-24 10:19:11 +0530

    Update ml_model.py

--- Day6-Machine-Learning/ml_model.py
+++ Day6-Machine-Learning/ml_model.py
@@ -1,20 +1,28 @@
 #prgrm15: First ML model
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+# load data
 data = pd.read_csv(r"C:\Users\KIIT0001\Desktop\AI-Internship -Journey-2026\Day6-Machine-Learning\students.csv")
 print("Students data:\n", data)
+# features(inputs)
 x = data[["science", "english"]]
+# label(output)
 y = data["math"]
-from sklearn.model_selection import train_test_split
+# split data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
-from sklearn.linear_model import LinearRegression
+# train model
 model = LinearRegression()
 model.fit(x_train, y_train)
+# predictions
 predictions = model.predict(x_test)
+# evaluate model
+error = mean_absolute_error(y_test, predictions)
 print("Predicted:", predictions)
 print("Actual:", list(y_test))
-from sklearn.metrics import mean_absolute_error
-error = mean_absolute_error(y_test, predictions)
 print("Mean Absolute Error:", error)
+# custom prediction
 sample = pd.DataFrame([[80,85]], columns = ["science", "english"])
 prediction = model.predict(sample)
 print("Predicted math score:", prediction)
