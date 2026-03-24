aee759c0cfd19858013697fcea2f0cf23aceb5be
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-24 09:33:51 +0530

    Update ml_model.py

--- Day6-Machine-Learning/ml_model.py
+++ Day6-Machine-Learning/ml_model.py
@@ -2,13 +2,16 @@
 import pandas as pd
 data = pd.read_csv(r"C:\Users\KIIT0001\Desktop\AI-Internship -Journey-2026\Day6-Machine-Learning\students.csv")
 print("Students data:\n", data)
-data["total"] = data["math"] + data["sciene"] + data["english"]
-x = data[["math", "science", "english"]]
-y = data["total"]
+x = data[["science", "english"]]
+y = data["math"]
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
-model.fit(x,y)
+model.fit(x_train, y_train)
 import pandas as pd
 new_data = pd.DataFrame([[80, 85, 82]], columns = ["math", "science", "english"])
-prediction = model.predict(new_data)
-print("Predicted math scores:", prediction)
+prediction = model.predict(x_train)
+print("Predicted:", prediction)
+print("Actual:", list(y_test))
+
