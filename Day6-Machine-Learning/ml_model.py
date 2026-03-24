04e8ccf94af661ef3f25fc585195aab1329467c2
Sanya Singhania <singhaniakuuk@gmail.com>
2026-03-24 10:06:52 +0530

    Update ml_model.py

--- Day6-Machine-Learning/ml_model.py
+++ Day6-Machine-Learning/ml_model.py
@@ -9,13 +9,12 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(x_train, y_train)
-import pandas as pd
 predictions = model.predict(x_test)
 print("Predicted:", predictions)
 print("Actual:", list(y_test))
 from sklearn.metrics import mean_absolute_error
 error = mean_absolute_error(y_test, predictions)
 print("Mean Absolute Error:", error)
-sample = [[80,85]]
+sample = pd.DataFrame([[80,85]], columns = ["science", "english"])
 prediction = model.predict(sample)
 print("Predicted math score:", prediction)
